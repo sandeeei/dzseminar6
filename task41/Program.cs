7fb9b7ddfbbc8dc870e4938f9484460289185e41
Sergei <sandeeey@example.com>
2023-03-03 12:44:07 +0300

    task_41

---
+++ task41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество элементов массива: ");
+int size =Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+//заполнение массива пользователеь
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"Введите элемент массива под индексом  {i}: \t");
+    array[i]=Convert.ToInt32(Console.ReadLine());
+}
+
+//Подщет чисел больше 0 в массиве
+
+int count = 0;
+
+for (int i = 0; i < size; i++)
+{
+    if (array[i]>0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"\n Введенныйе числа : [{String.Join("; ", array)}]");
+Console.WriteLine($"\n Вы ввели   {count} чисел больше 0");
